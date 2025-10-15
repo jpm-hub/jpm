681cd6b7350b29d0f86b57a27d83ca028e513aa7
SunnyKentz <94718699+SunnyKentz@users.noreply.github.com>
2025-10-15 13:05:45 -0400

    Modify jpx.cmd to handle arguments and call bash

--- jpx/jpx.cmd
+++ jpx/jpx.cmd
@@ -1,9 +1,11 @@
 @echo off
 setlocal
 set "PATH=%PATH%;%CD%\jpm_dependencies\execs"
-if "%*"=="" (
-	jpm
+set arg_count=0
+for %%x in (%*) do set /a arg_count+=1
+if %arg_count%==0 (
+        jpm
 ) else (
-	call %*
+        "C:\Program Files\Git\bin\bash.exe" %*
 )
 endlocal
