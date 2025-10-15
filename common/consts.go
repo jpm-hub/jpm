dc84cd52a7cd5b7a9200607ddb503b962346384e
SunnyKentz <94718699+SunnyKentz@users.noreply.github.com>
2025-10-15 15:09:25 -0400

    Update VERSION constant to 1.0.13

--- common/consts.go
+++ common/consts.go
@@ -1,4 +1,4 @@
 package common
 
-const VERSION = "1.0.12"
+const VERSION = "1.0.13"
 const JPM_REPO_API = "https://raw.githubusercontent.com/jpm-hub/repo/refs/heads/main/" //"http://localhost:9090/"
