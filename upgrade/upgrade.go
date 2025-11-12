2b0acd4b0c06bf248079d441a51daa1ae9c9e702
Sunny Derisme <sderisme@hme.com>
2025-11-12 18:46:48 -0500

    save

--- upgrade/upgrade.go
+++ upgrade/upgrade.go
@@ -12,18 +12,15 @@ func Upgrade() {
 		if err != nil {
 			return
 		}
-		defer os.Remove(tempFile.Name())
 		tempFile.Close()
-
-		COM.RunCMD(fmt.Sprintf(`curl -L -o "%s" https://cmd.jpmhub.org && start "%s"`, tempFile.Name(), tempFile.Name()), true)
+		println(tempFile.Name())
+		COM.RunCMD(fmt.Sprintf(`curl -L -o "%s" https://cmd.jpmhub.org && start "upgrade" "C:\WINDOWS\system32\cmd.exe" /param="/c %s"`, tempFile.Name(), tempFile.Name()), true)
 	} else {
-		tempFile, err := os.CreateTemp("", "upgrade_jpm*.sh")
-		if err != nil {
-			return
-		}
-		defer os.Remove(tempFile.Name())
-		tempFile.Close()
-		COM.RunScript(fmt.Sprintf("curl -L -o %s https://sh.jpmhub.org && sh %s", tempFile.Name(), tempFile.Name()), true)
+		// Simple background upgrade for Unix
+		pid := os.Getpid()
+		script := fmt.Sprintf("(while kill -0 %d 2>/dev/null; do sleep 1; done; curl -L -o s.sh https://sh.jpmhub.org && sh s.sh) &", pid)
+		COM.RunScript(script, false)
+		fmt.Println("Upgrade scheduled to run after process exits.")
 	}
 
 }
