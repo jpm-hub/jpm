6b26dddf2e0106d23b4bc66b07cf38db4576a2a3
Sunny Derisme <sderisme@hme.com>
2025-11-14 18:28:33 -0500

    aded -bare to init

---
+++ init/bare.go
@@ -0,0 +1,27 @@
+package init
+
+import (
+	"fmt"
+	COM "jpm/common"
+	"os"
+	"strings"
+)
+
+func Bare(language string) {
+	if err := os.WriteFile("package.yml", []byte(COM.GetPackageTemplateSimple(language)), 0644); err != nil {
+		fmt.Printf("Error creating package.yml: %v\n", err)
+		os.Exit(1)
+	}
+	if strings.Contains(language, "kotlin") {
+		if err := os.WriteFile("App.kt", []byte(COM.GetKotlinAppTemplate("")), 0644); err != nil {
+			fmt.Printf("Error creating main Kotlin file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	if strings.Contains(language, "java") {
+		if err := os.WriteFile("App.java", []byte(COM.GetJava25AppTemplate()), 0644); err != nil {
+			fmt.Printf("Error creating main Java file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
