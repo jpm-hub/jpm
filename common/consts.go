d864948c1fe320776c2a56b89e8afff0e94c7e5b
sunny <derisme@live.ca>
2026-04-02 17:51:53 -0400

    v1.2.10

--- common/consts.go
+++ common/consts.go
@@ -1,6 +1,6 @@
 package common
 
-const VERSION = "1.2.9"
+const VERSION = "1.2.10"
 const KOTLIN_VERSION = "2.3.10"
 const JUNIT_STANDALONE_VERSION = "6.0.3"
 const JPM_REPO_API = "https://raw.githubusercontent.com/jpm-hub/repo/refs/heads/main/" //"http://localhost:9090/"
